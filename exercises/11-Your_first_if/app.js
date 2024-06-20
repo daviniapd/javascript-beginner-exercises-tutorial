3bdac7cee34ad1a8439fa1ae6daaade926b81f23
Davinia pd <delgadavi@gmail.com>
2024-06-20 13:08:02 +0000

    Pending changes exported from your codespace

--- exercises/11-Your_first_if/app.js
+++ exercises/11-Your_first_if/app.js
@@ -1,3 +1,9 @@
 let total = prompt('How many km are left to go?');
-
+if (total > 100) {
+    console.log("We still have a bit of driving left to go");
+} else if (total <= 50) {
+    console.log("I'm parking. I'll see you right now");
+} else {
+    console.log("We'll be there in 5 minutes");
+}
 // Your code below:
--- exercises/14-Random_numbers/app.js
+++ exercises/14-Random_numbers/app.js
@@ -1,7 +1,8 @@
 function getRandomInt()
 {
-	let randomNumber = Math.random();
+	let randomNumber = Math.floor((Math.random() * 10) + 1);
 	return randomNumber;
+
 }
 
 
